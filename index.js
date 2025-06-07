13d920012e7303b78b365b92bfa5397ae0f58973
Vijay Kumar Kushwaha <cmrsl.net@gmail.com>
2025-06-07 14:36:43 +0530

    chore: initialize project with package.json and express dependency

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("<u><h1 style='color: #2c3e50;'> Hello Vijay... This is the Home Page. </h1></u>");
+});
+
+app.get("/vijay", (req, res) =>{
+    res.send("<h3> This is your page Vijay</h3>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
